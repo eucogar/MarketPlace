0a86a57f56e5e5f9457a16a91db008a5c0a374cf
eucogar <79609961+eucogar@users.noreply.github.com>
2023-03-12 11:36:06 -0500

    BackandFron

--- src/services/Images.ts
+++ src/services/Images.ts
@@ -1,22 +1,26 @@
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
-export const Galery = async () => {
+export const Galery = async (image: string[], onChange: Function) => {
   try {
-    await launchImageLibrary({mediaType: 'photo'}, response => {
-      if (response.didCancel) {
-        console.log('Usuário cancelou a seleção de imagem');
-      }
-    });
+    await launchImageLibrary(
+      {mediaType: 'photo', selectionLimit: 4},
+      (response: any) => {
+        let imageCopy: string[] = image ? image : [];
+        imageCopy.push(response.assets[0].uri);
+        onChange(imageCopy, 'image');
+      },
+    );
   } catch (error) {
     console.log(error);
   }
 };
-export const Camera = async () => {
+
+export const Camera = async (image: string[], onChange: Function) => {
   try {
-    await launchCamera({mediaType: 'photo'}, response => {
-      if (response.didCancel) {
-        console.log('Usuário cancelou a seleção de imagem');
-      }
+    await launchCamera({mediaType: 'photo'}, (response: any) => {
+      let imageCopy: string[] = image ? image : [];
+      imageCopy.push(response.assets[0].uri);
+      onChange(imageCopy, 'image');
     });
   } catch (error) {
     console.log(error);
